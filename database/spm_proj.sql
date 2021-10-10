01f1e649810a8b91e280e3f84e443a43cd620e56
depressed-9987 <51493022+gerry-9987@users.noreply.github.com>
2021-10-11 02:51:08 +0800

    Update spm_proj.sql
    Populated the rest of the tables with dummy data

--- database/spm_proj.sql
+++ database/spm_proj.sql
@@ -72,7 +72,6 @@ DROP TABLE IF EXISTS GRADEDQUIZ;
 CREATE TABLE GRADEDQUIZ
 (
     quizID int(11) NOT NULL,
-    quizScore int(11) NOT NULL,
     passingScore int(11) NOT NULL,
     CONSTRAINT gradedquiz_pk PRIMARY KEY (quizID),
     CONSTRAINT gradedquiz_fk FOREIGN KEY (quizID) REFERENCES QUIZ(quizID)
@@ -153,6 +152,17 @@ CREATE TABLE QUIZ_QUESTION
     CONSTRAINT quiz_question_fk2 FOREIGN KEY (questionID) REFERENCES QUESTION(questionID)
 );
 
+DROP TABLE IF EXISTS LEARNER_QUIZ;
+CREATE TABLE LEARNER_QUIZ
+(
+    quizID int(11) NOT NULL,
+    staffID int(11) NOT NULL,
+    quizScore int(11) NOT NULL,
+    CONSTRAINT learner_quiz_pk PRIMARY KEY (quizID, staffID),
+    CONSTRAINT learner_quiz_fk FOREIGN KEY (quizID) REFERENCES QUIZ(quizID),
+    CONSTRAINT learner_quiz_fk2 FOREIGN KEY (staffID) REFERENCES LEARNER(staffID)
+);
+
 INSERT INTO STAFF VALUES
 (1, 'gerry', 'Geraldine', 'Learner'),
 (2, 'ly', 'Ley Yi', 'Learner'),
@@ -194,3 +204,148 @@ INSERT INTO CLASS VALUES
 (16, 7, '2021-12-21', '2022-02-28', '22:30:00', '23:30:00', 2, 'Haoyue', 3),
 (17, 7,  '2021-12-21', '2022-02-28', '12:30:00', '13:30:00', 2, 'Haoyue', 3),
 (18, 8, '2021-12-21', '2022-02-28', '22:30:00', '23:30:00', 2, 'Jewel', 4);
+
+INSERT INTO QUIZ VALUES
+(1, '2021-01-01', '2021-01-03'),
+(2, '2021-01-01', '2021-01-03'),
+(3, '2021-01-01', '2021-01-03'),
+(4, '2021-01-01', '2021-01-03'),
+(5, '2021-01-03', '2021-02-07'),
+(6, '2021-01-03', '2021-02-07'),
+(7, '2021-01-03', '2021-02-07'),
+(8, '2021-01-03', '2021-02-07'),
+(9, '2021-01-03', '2021-02-07'),
+(10, '2021-02-07', '2021-12-21'),
+(11, '2021-02-07', '2021-12-21'),
+(12, '2021-02-07', '2021-12-21'),
+(13, '2021-02-07', '2021-12-21'),
+(14, '2021-02-07', '2021-12-21'),
+(15, '2021-02-07', '2021-12-21'),
+(16, '2021-12-21', '2022-02-28'),
+(17, '2021-12-21', '2022-02-28'),
+(18, '2021-12-21', '2022-02-28');
+
+INSERT INTO QUESTION VALUES
+(1, 'Is cat cute?', 'True'),
+(2, 'Is dog cute?', 'True'),
+(3, 'Is turtle cute?', 'True'),
+(4, 'Is life cute?', 'False'),
+(5, 'Is foetus cute?', 'False'),
+(6, 'Is baby cute?', 'True'),
+(7, 'Is diluc cute?', 'True'),
+(8, 'Is zhongli cute?', 'True'),
+(9, 'Which cat is cutest?', 'B'),
+(10, 'Which dog is cuter?', 'C'),
+(11, 'AAA', 'D'),
+(12, 'BBB', 'B'),
+(13, 'BBV', 'C'),
+(14, 'CCC', 'True'),
+(15, 'DDD', 'F'),
+(16, 'EEE', 'E'),
+(17, 'FFF', 'False'),
+(18, 'GGG', 'G');
+
+INSERT INTO CHAPTER VALUES
+(1, 'CAT', 1),
+(2, 'DOG', 2),
+(3, 'TURTLE', 3),
+(4, 'LIFE', 4),
+(5, 'foetus', 5),
+(6, 'baby', 6),
+(7, 'diluc', 7),
+(8, 'zhongli', 8),
+(9, 'cutest', 9),
+(10, 'cuter', 10),
+(11, 'AAA', 11),
+(12, 'BBB', 12),
+(13, 'BBV', 13),
+(14, 'CCC', 14),
+(15, 'DDD', 15),
+(16, 'EEE', 16),
+(17, 'FFF', 17),
+(18, 'GGG', 18);
+
+INSERT INTO GRADEDQUIZ VALUES
+(1, 50),
+(2, 50),
+(3, 50),
+(4, 50),
+(5, 50),
+(6, 50),
+(7, 50);
+
+INSERT INTO CLASS_CHAPTER VALUES
+(1, 1, 1),
+(1, 1, 2);
+
+INSERT INTO TRAINER VALUES
+(3, 10),
+(4, 8);
+
+INSERT INTO LEARNER VALUES
+(1, 1),
+(2, 1),
+(6, 1),
+(7, 1),
+(8, 1),
+(9, 1);
+
+INSERT INTO TEACH_CLASS VALUES
+(3, 1, 1),
+(3, 1, 2),
+(3, 2, 3),
+(3, 2, 4),
+(4, 2, 5),
+(4, 2, 6),
+(4, 3, 7),
+(3, 4, 8),
+(3, 4, 9),
+(3, 4, 10),
+(4, 5, 11),
+(4, 5, 12),
+(4, 6, 13),
+(4, 6, 14),
+(3, 7, 15),
+(3, 7, 16),
+(3, 7, 17),
+(4, 8, 18);
+
+INSERT INTO MATERIAL VALUES
+(1, "aaa", "document", "https://cseweb.ucsd.edu/classes/sp15/cse190-c/reports/sp15/048.pdf", "Info", 1),
+(2, "bbb", "link", "www.google.com", "Website", 2),
+(3, "bbb", "video", "https://www.youtube.com/watch?v=mFFXuXjVgkU&ab_channel=DevOpsJourney", "Vid", 3);
+
+INSERT INTO TAKE_CLASS VALUES
+(1, 1, 1),
+(2, 1, 2),
+(6, 2, 3),
+(7, 2, 4),
+(8, 2, 5),
+(9, 2, 6);
+
+INSERT INTO QUIZ_QUESTION VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(2, 1),
+(2, 2),
+(2, 3),
+(2, 4),
+(3, 1);
+
+INSERT INTO LEARNER_QUIZ VALUES
+(1, 1, 23),
+(2, 1, 14),
+(3, 1, 46),
+(1, 2, 22),
+(2, 2, 45),
+(3, 2, 23),
+(3, 6, 24),
+(2, 7, 15),
+(1, 8, 50);
+
+
+
+
+
