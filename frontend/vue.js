b1e6bbf9420939e7dc2df401f10be80983f5393f
Wesleyongs <72511481+Wesleyongs@users.noreply.github.com>
2021-10-15 23:03:40 +0800

    Frontend

---
+++ frontend/vue.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: "#app ",
+    computed: {},
+    data: {
+        course_categories: [
+            "Mechanical Engineer ",
+            "Computer Engineer ",
+            "Chemical Engineer ",
+        ],
+        trainers: [
+            "Wesley", "Jewel", "Gerry", "HaoYue", "LY"
+        ]
+    },
+});
