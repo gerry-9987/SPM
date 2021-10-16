d4d2b8345f28cb4d48fd5ce0ca225dff49108b65
Wesleyongs <72511481+Wesleyongs@users.noreply.github.com>
2021-10-16 09:16:43 +0800

    Stuff

--- frontend/vue.js
+++ frontend/vue.js
@@ -9,6 +9,17 @@ var app = new Vue({
         ],
         trainers: [
             "Wesley", "Jewel", "Gerry", "HaoYue", "LY"
-        ]
+        ],
+        form_course_name: 'Enter course name',
+        form_course_category: 0,
+        form_number_of_classes: 0,
+        form_course_capacity: 0,
+        form_course_trainers: []
     },
+    methods: {
+        submit_new_course: function() {
+            console.log('I am clicked')
+            console.log(this.form_course_name, this.form_course_category, this.form_number_of_classes, this.form_course_capacity, this.form_course_trainers)
+        }
+    }
 });
