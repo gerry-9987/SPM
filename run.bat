c9942be456fe46789322956430b865b759369f34
depressed-9987 <51493022+gerry-9987@users.noreply.github.com>
2021-10-19 02:46:28 +0800

    Create run.bat
    Now you can use run run.bat to run all the backend files at once :D

---
+++ run.bat
@@ -0,0 +1,7 @@
+start "chapter" python backend-api/chapter.py
+start "class" python backend-api/class.py
+start "class chapter" python backend-api/classChapter.py
+start "course" python backend-api/course.py
+start "learner" python backend-api/learner.py
+start "staff" python backend-api/staff.py
+start "trainer" python backend-api/trainer.py
