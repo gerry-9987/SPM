d6440841f7046490e3d3203f3331963d258f3606
jeweltan27 <jewelxyt@gmail.com>
2021-10-20 15:01:48 +0800

    [Backend] Update run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
 start "chapter" python backend-api/chapter.py
 start "class" python backend-api/class.py
-start "class chapter" python backend-api/classChapter.py
+start "classChapter" python backend-api/classChapter.py
 start "course" python backend-api/course.py
 start "learner" python backend-api/learner.py
 start "staff" python backend-api/staff.py
 start "trainer" python backend-api/trainer.py
-start "take_class" python backend-api/take_class.py
+start "takeClass" python backend-api/takeClass.py
