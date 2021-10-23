a11014fca6de62e3514c04ac54aa85a6a484206e
depressed-9987 <51493022+gerry-9987@users.noreply.github.com>
2021-10-24 00:39:46 +0800

    Update database
    Got rid of questions table and adjusted quiz table, created link of graded quiz to class and tidied up some parts

--- run.bat
+++ run.bat
@@ -2,6 +2,7 @@ start "chapter" python backend-api/chapter.py
 start "class" python backend-api/class.py
 start "classChapter" python backend-api/classChapter.py
 start "course" python backend-api/course.py
+start "quiz" python backend-api/quiz.py
 start "learner" python backend-api/learner.py
 start "staff" python backend-api/staff.py
 start "trainer" python backend-api/trainer.py
