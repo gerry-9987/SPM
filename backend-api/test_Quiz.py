501b89b6f8e39c6b2a4deade84d5a050556bb3b2
depressed-9987 <51493022+gerry-9987@users.noreply.github.com>
2021-10-30 20:05:10 +0800

    Added TDD for quiz

---
+++ backend-api/test_Quiz.py
@@ -0,0 +1,31 @@
+import unittest
+
+from dbModel import *
+from quiz import *
+
+
+class test_Quiz(unittest.TestCase):
+    def setUp(self):
+        self.quiz = Quiz(7, '01 Jan 2021', '03 Jan 2021', 'Is cat cute?', 'True')
+
+    def tearDown(self):
+        self.quiz = None
+
+    def test_quiz_details(self):
+        quizDetails = self.quiz.json()
+        self.assertEqual(quizDetails, {
+            "quizID": 7,
+            "startDate": '01 Jan 2021',
+            "endDate": '03 Jan 2021',
+            "question": 'Is cat cute?',
+            "answer": 'True'}
+        )
+
+    # def test_quiz_details(self):
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
